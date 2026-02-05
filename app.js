2101211433996bc0ae8c61c08b471132531232dc
Steve W <58841433+zichsw@users.noreply.github.com>
2026-02-05 16:18:09 -0500

    will fail

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error')
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
